8ca85332c48672393e3ae1856f5073e995840c8e
agusrypstra <agusrypstra@gmail.com>
2025-04-16 21:28:45 -0300

    Categoria pasada a entidad

---
+++ src/main/java/com/todoCompras/backend/model/Categoria.java
@@ -0,0 +1,54 @@
+package com.todoCompras.backend.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_padre_id")
+    private Categoria categoriaPadre;
+
+    @OneToMany(mappedBy = "categoriaPadre", cascade = CascadeType.ALL)
+    private List<Categoria> subcategorias = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categoria getCategoriaPadre() {
+        return categoriaPadre;
+    }
+
+    public void setCategoriaPadre(Categoria categoriaPadre) {
+        this.categoriaPadre = categoriaPadre;
+    }
+
+    public List<Categoria> getSubcategorias() {
+        return subcategorias;
+    }
+
+    public void setSubcategorias(List<Categoria> subcategorias) {
+        this.subcategorias = subcategorias;
+    }
+}
--- src/main/java/com/todoCompras/backend/model/enums/CategoriaLocal.java
+++
@@ -1,7 +0,0 @@
-package com.todoCompras.backend.model.enums;
-
-public enum CategoriaLocal {
-    LOCAL,
-    EMPRENDIMIENTO,
-    OFICIO
-}
