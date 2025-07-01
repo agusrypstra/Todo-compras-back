f68925a5c8292b3d70520e5c95fb813ba5565fc8
agusrypstra <agusrypstra@gmail.com>
2025-07-01 19:55:03 -0300

    Repository actualizado

--- src/main/java/com/todoCompras/backend/repository/CategoriaRepository.java
+++ src/main/java/com/todoCompras/backend/repository/CategoriaRepository.java
@@ -2,12 +2,24 @@ package com.todoCompras.backend.repository;
 
 import com.todoCompras.backend.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     List<Categoria> findByPadreIsNull(); // Para padres
+    List<Categoria> findByPadreIsNotNull(); // Para padres
     List<Categoria> findByPadreId(Long id);
     Optional<Categoria> findByNombre(String nombre);
+
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END " +
+           "FROM Categoria c WHERE c.nombre = :nombre AND c.padre.id = :padreId")
+    boolean existsByNombreAndPadreId(
+        @Param("nombre") String nombre,
+        @Param("padreId") Long padreId
+    );
+
+
 }
