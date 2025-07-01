8b43ae4c252b82357d7a01f3b6d9c30d51126f12
agusrypstra <agusrypstra@gmail.com>
2025-07-01 19:55:57 -0300

    Feat: obtener locales y locales por categoria agregados

--- src/main/java/com/todoCompras/backend/service/LocalService.java
+++ src/main/java/com/todoCompras/backend/service/LocalService.java
@@ -1,11 +1,14 @@
 package com.todoCompras.backend.service;
 
-
+import com.todoCompras.backend.dto.local.LocalResponseDTO;
 import com.todoCompras.backend.dto.solicitudes.SolicitudRegistroLocalRequestDTO;
 import com.todoCompras.backend.mapper.LocalMapper;
 import com.todoCompras.backend.model.Local;
 import com.todoCompras.backend.repository.LocalRepository;
-import com.todoCompras.backend.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +22,23 @@ public class LocalService {
         this.localRepository = localRepository;
         this.localMapper = localMapper;
     }
+    public List<LocalResponseDTO> obtenerLocales() {
+        List<Local> locales = localRepository.findAll();
+        return locales.stream()
+                .map(localMapper::toResponseDTO)
+                .collect(Collectors.toList());
+    }
+    public List<Local> obtenerLocalPorCategoriaId(Long categoriaId) {
+        return localRepository.obtenerLocalPorCategoriaId(categoriaId);
+    }
 
     public Local registrarLocal(SolicitudRegistroLocalRequestDTO dto) {
         Local local = localMapper.toEntity(dto);
         return localRepository.save(local);
+    }
+    public Local findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
     }
 
 }
