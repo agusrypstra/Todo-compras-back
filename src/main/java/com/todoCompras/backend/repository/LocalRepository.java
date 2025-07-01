7578bd54c8996fb163cb61c49e110a5bdcb7da13
agusrypstra <agusrypstra@gmail.com>
2025-07-01 19:55:30 -0300

    Repositoy con consulta por categoria

--- src/main/java/com/todoCompras/backend/repository/LocalRepository.java
+++ src/main/java/com/todoCompras/backend/repository/LocalRepository.java
@@ -1,9 +1,17 @@
 package com.todoCompras.backend.repository;
 
 import com.todoCompras.backend.model.Local;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocalRepository extends JpaRepository<Local, Long> {
+    @Query("SELECT l FROM Local l JOIN FETCH l.categoria c WHERE c.id = :categoriaId")
+    List<Local> obtenerLocalPorCategoriaId(@Param("categoriaId") Long categoriaId);
+    
 }
